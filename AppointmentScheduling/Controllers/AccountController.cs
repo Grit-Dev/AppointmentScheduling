3066a1c6e4f49a66caa1d7e615a11b218cfab5b0
Paul McGinley <Paulmcginley16@outlook.com>
2022-07-20 10:24:33 +0100

    Add project files.

---
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using AppointmentScheduling.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
